7715690768e6b3f6638f44494ef159ae86856426
lilcaesar <lilcaesar4@gmail.com>
2018-07-05 17:08:44 +0200

    aggiunto scorrimento file nella cartella

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <experimental/filesystem>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string path = "/root/Scrivania/DecisionScienceProjectData";
+
+    for(auto & p : std::experimental::filesystem::directory_iterator(path)){
+        string line;
+        std::ifstream fs (p.path().string());
+
+        while(getline(fs, line)){
+            cout << line << endl;
+        }
+
+        fs.close();
+    }
     return 0;
 }
