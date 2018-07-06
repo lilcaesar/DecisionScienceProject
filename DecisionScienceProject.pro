e034dfaf11dbc254bfaaa8a8a6be6bca8aa9635a
lilcaesar <lilcaesar4@gmail.com>
2018-07-06 12:03:17 +0200

    Iniziato il parsing

--- DecisionScienceProject.pro
+++ DecisionScienceProject.pro
@@ -1,7 +1,12 @@
 TEMPLATE = app
-CONFIG += console c++11
+CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
+
+CONFIG += c++17
+QMAKE_CXXFLAGS += -std=c++17
+
+LIBS += -lstdc++fs
 
 SOURCES += \
         main.cpp \
--- main.cpp
+++ main.cpp
@@ -7,15 +7,36 @@ using namespace std;
 
 int main()
 {
+    std::vector<std::pair<int,int>> coordinates;
     string path = "/root/Scrivania/DecisionScienceProjectData";
 
     for(auto & p : std::experimental::filesystem::directory_iterator(path)){
         string line;
         std::ifstream fs (p.path().string());
+        int dimension;
+        int capacity;
+        istringstream tokenStream;
+        string token;
 
-        while(getline(fs, line)){
-            cout << line << endl;
+        //Skipping 3 lines and using the 4th
+        for(int i=0; i<4;i++){
+            getline(fs, line);
         }
+        //parsing the 4th line
+        tokenStream.str(line);
+        for(int i=0; i<3; i++){
+            getline(tokenStream, token, ' ');
+        }
+        dimension = stoi(token);
+
+        //skipping 3 lines
+        for(int i=0; i<3;i++){
+            getline(fs, line);
+        }
+
+        //parsing the coordinates data
+        tokenStream.clear();
+        tokenStream.str(line);
 
         fs.close();
     }
