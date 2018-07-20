1adf310c803cf14db51b68319a3366255b2f1e80
lilcaesar <lilcaesar4@gmail.com>
2018-07-20 15:09:01 +0200

    aggiunta stampa valori dei file

--- main.cpp
+++ main.cpp
@@ -11,6 +11,23 @@ int main()
 
     for(const std::experimental::filesystem::directory_entry & p : std::experimental::filesystem::directory_iterator(path)){
         parseInitialFile(p, coordinates, demand);
+
+        std::cout << p << std::endl;
+
+        for (auto const& value : coordinates)
+        {
+            std::cout << value.first << "  " << value.second << std::endl;
+            //std::cout << value << std::endl;
+        }
+
+        for (auto const& value : demand)
+        {
+            std::cout << value << std::endl;
+        }
+
+        coordinates.clear();
+        demand.clear();
     }
+
     return 0;
 }
